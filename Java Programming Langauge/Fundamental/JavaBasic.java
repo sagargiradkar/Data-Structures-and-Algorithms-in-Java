050cc669f904f37d97adfb127ab5c13f5a53c8fd
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-09-07 19:08:38 +0530

    First Program Added

--- Java Programming Langauge/Fundamental/JavaBasic.java
+++ Java Programming Langauge/Fundamental/JavaBasic.java
@@ -3,5 +3,11 @@ public class JavaBasic{
     public static void main(String args[]){
         System.out.println("Hellow World");
         System.out.println("Welcome To the World Of Java Programming");
+        System.out.println("Welcome To the World Of Java Programming"); 
+        System.out.println("Welcome To the World Of Java Programming");
+        System.out.println("Welcome To the World Of Java Programming");
+        System.out.println("Welcome To the World Of Java Programming");
+        System.out.println("Welcome To the World Of Java Programming"); 
+        System.out.println("Welcome To the World Of Java Programming");
     }
 }
