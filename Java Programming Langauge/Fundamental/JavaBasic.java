75b79b84447d3761deb45a79c0dedb17f90e0e5a
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-09-07 19:07:31 +0530

    Java

--- Java Programming Langauge/Fundamental/JavaBasic.java
+++ Java Programming Langauge/Fundamental/JavaBasic.java
@@ -2,5 +2,6 @@
 public class JavaBasic{
     public static void main(String args[]){
         System.out.println("Hellow World");
+        System.out.println("Welcome To the World Of Java Programming");
     }
 }
