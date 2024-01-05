fed455fe2b98260071995d63154b28a2aa4d154c
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-01-05 21:01:47 +0530

    Java class 4

--- OOPS/src/Main.java
+++ OOPS/src/Main.java
@@ -59,9 +59,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		double d = 1.7976931348623157E308;
-		System.out.println("Size of Double is :"+Double.SIZE);
-		System.out.println("MINVALUE of Double is ::"+ Double.MIN_VALUE);
-		System.out.println("MAXVALUE of Double is ::"+ Double.MAX_VALUE);
+//		System.out.println("Size of Double is :"+Double.SIZE);
+//		System.out.println("MINVALUE of Double is ::"+ Double.MIN_VALUE);
+//		System.out.println("MAXVALUE of Double is ::"+ Double.MAX_VALUE);
+//		
+//		double a = 20;
+//		double b = 0;
+//		double c = a/b;
+//		double e = a/b;
+//		System.out.println(e);
+//		System.out.println(c);
+//		byte num1 = 45;
+//		double num2;
+//				num2 = num1;
+		int a = 5;
+		a++;
+		++a;
+		System.out.println(a);
+				
 	
 		
 	}
