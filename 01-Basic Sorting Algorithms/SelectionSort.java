0fb2213ab319815b323182264f77a44d5cb12228
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-06-24 07:37:55 +0530

    space btn element

--- 01-Basic Sorting Algorithms/SelectionSort.java
+++ 01-Basic Sorting Algorithms/SelectionSort.java
@@ -16,7 +16,7 @@ public class SelectionSort {
     }
     public static void display(int arr[]){
         for(int i=0 ; i<arr.length ;i++){
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + "  ");
         }
     }
     public static void main(String[] args){
