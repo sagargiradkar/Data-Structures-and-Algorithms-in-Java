28e5396f1db189bbd1c6ed2daf5efed58f243409
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-07-16 23:02:42 +0530

    Count Sort

--- 01-Basic Sorting Algorithms/CountSort.java
+++ 01-Basic Sorting Algorithms/CountSort.java
@@ -6,18 +6,26 @@ public class CountSort {
         {
             largest = Math.max(largest, arr[i]);
         }
-        int count [] = new int[largest + 1];
 
+
+        int count [] = new int[largest + 1];
+        for(int i=0 ; i<arr.length; i++)
+        {
+           count[arr[i]]++;
+        }
+
+        //Sorting
         int j = 0;
-        for(int i=count.length-1;i>=0 ;i--)
+        for(int i=0 ;i<count.length;i++)
         {
             while(count[i] > 0)
             {
                 arr[j] = i;
                 j++;
-                count[i]--;
+                count[i] --;
             }
         }
+
     }
     public static void display(int arr[]){
         for(int i=0 ; i<arr.length ;i++){
