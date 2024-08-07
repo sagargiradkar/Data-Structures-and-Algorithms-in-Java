97ef4e9b2277031c01bdd9fafce4d576c08cb641
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-08-08 00:20:39 +0530

    Hollow Pyramid

---
+++ Pattern/HollowInvertedPyramid.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+
+public class HollowInvertedPyramid {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number ::");
+        n = sc.nextInt();
+        for(int i = 0 ;i<n;i++)
+        {
+            for(int j =0;j<n;j++)
+            {
+                if(i==0||j==0||j==n-i-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
