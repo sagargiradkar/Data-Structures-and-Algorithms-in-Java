906f2a7347e0e9d5ee99bd808a2633cf46c75d1a
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-08-07 16:36:54 +0530

    Queue using link list

---
+++ 05-Queue/OueueLL.java
@@ -0,0 +1,73 @@
+public class OueueLL {
+    static class Node 
+    {
+        int data ;
+        Node next;
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class QueueL{
+        static Node head = null;
+        static Node tail = null;
+
+        public static boolean isEmplty()
+        {
+            return head == null && tail == null;
+        }
+
+        public static void add(int data)
+        {
+            Node newNode = new Node(data);
+
+            if(head == null)
+            {
+                head = tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+        public static int remove()
+        {
+            if(isEmplty())
+            {
+                System.out.println("empty queue");
+                return -1;
+            }
+            int front = head.data;
+
+            if(tail == head ){
+                tail = head = null;
+            }else
+            {
+                head = head.next;
+            }
+            return front;
+        }
+        public static int peek()
+        {
+            if(isEmplty())
+            {
+                System.out.println("empty Queue");
+                return -1;
+            }
+            return head.data;
+        }
+
+    }
+    public static void main(String[] args) {
+        QueueL q = new QueueL();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmplty())
+        {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
