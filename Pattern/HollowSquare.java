0c85cff5a048777d17d57415f55ac9709cacb92a
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-08-08 00:13:05 +0530

    Hollow Squre

---
+++ Pattern/HollowSquare.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class HollowSquare {
+    public static void main(String[] args) {
+        int n ;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for(int j = 0 ; j<n ;j++)
+            {
+                if(i==0 || i==n-1 || j==0 || j==n-1)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
