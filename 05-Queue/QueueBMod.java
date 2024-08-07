d6286559eff696589dcc5c46558d3bf154315856
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-08-07 16:21:26 +0530

    Que

--- 05-Queue/QueueBMod.java
+++ 05-Queue/QueueBMod.java
@@ -1,11 +1,10 @@
 public class QueueBMod {
 
-    static class Queue
-    {
-        static int arr[] ;
-        static int size;
-        static int rear;
-        static int front;
+    static class Queue {
+        int arr[];
+        int size;
+        int rear;
+        int front;
 
         public Queue(int n) {
             arr = new int[n];
@@ -13,72 +12,65 @@ public class QueueBMod {
             rear = -1;
             front = -1;
         }
-        
-        public static boolean isEmplty()
-        {
+
+        public boolean isEmpty() {
             return rear == -1 && front == -1;
         }
 
-        public static boolean isFull()
-        {
-            return (rear+1)%size == front;
+        public boolean isFull() {
+            return (rear + 1) % size == front;
         }
-        public static void add(int data)
-        {
-            if(isFull())
-            {
+
+        public void add(int data) {
+            if (isFull()) {
                 System.out.println("Queue is full");
                 return;
             }
 
-            if(front == -1)
-            {
+            if (front == -1) {
                 front = 0;
             }
-            rear = (rear + 1)%size;
+            rear = (rear + 1) % size;
             arr[rear] = data;
         }
 
-        public static int remove()
-        {
-            if(isEmplty())
-            {
+        public int remove() {
+            if (isEmpty()) {
                 System.out.println("Queue is empty");
                 return -1;
             }
-            
+
             int result = arr[front];
-            front = (front + 1)%size;
-            //last ele delete
-            if(rear == front )
-            {
+
+            // Check if last element is deleted
+            if (front == rear) {
                 rear = front = -1;
-            }
-            else
-            {
-                front = (front + 1)% size;
+            } else {
+                front = (front + 1) % size;
             }
             return result;
         }
-        public static int peek()
-        {
-            if(isEmplty())
-            {
+
+        public int peek() {
+            if (isEmpty()) {
                 System.out.println("Queue is empty");
                 return -1;
             }
             return arr[front];
         }
-
     }
+
     public static void main(String[] args) {
-        Queue q = new Queue(5);
+        Queue q = new Queue(3);
         q.add(1);
         q.add(2);
         q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
 
-        while(!q.isEmplty())
-        {
+        while (!q.isEmpty()) {
             System.out.println(q.peek());
             q.remove();
         }
