e64b6c71916d76b1593ef1053620d35482f132a6
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-08-08 14:25:41 +0530

    Reverse A number

---
+++ BasicProgram/ReverseNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int ans =0,rem=0;
+        int num = 12345;
+        while(num > 0)
+        {
+            int digit = num % 10 ;
+            ans = ans *10 + digit;
+            num = num /10;
+        }
+        System.out.println(ans);
+    }
+}
