7cf5e517de0d5a6beaa744366065eeac2e3b2a18
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-08-08 13:41:11 +0530

    NumericPalindrome

---
+++ Pattern/HalfDiamond.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class HalfDiamond {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number ::");
+        n = sc.nextInt();
+        for(int i=0;i<2*n-1;i++)
+        {
+            int cond=0;
+            if(i<n)
+            {
+                cond=i;
+            }else{
+                cond = n - (i%n) - 2;
+            }
+            for(int j=0 ;j<=cond;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
