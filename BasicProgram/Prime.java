cb7377e2b24a5ce0913aa131189cdb1fbe1fbbee
sagargiradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-08-08 14:00:34 +0530

    Prime

--- BasicProgram/Prime.java
+++ BasicProgram/Prime.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
 
 public class Prime {
     public static void main(String[] args) {
-        int n = 11;
+
         int i = 2;
-        while(i<n)
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a nnumber :: ");
+        n = sc.nextInt();
+        
+        for(i=2;i<=n;i++)
         {
-            if(n%i==0){
-                System.out.println("Not Prime");
-                i++;
+            if(n%i == 0)
+            {
+                System.out.println("Not a prime");
+                break;
+            }
+            else{
+                System.out.println("Prime number");
+                break;
             }
         }
-        System.out.println("No is prime");
     }
 }
