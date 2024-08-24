d4af1f44966b93854f61506403d4fbcfd9d0f9b8
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-08-24 09:00:12 +0530

    delete at nth node

--- 03-Linked List/LinkedLists.java
+++ 03-Linked List/LinkedLists.java
@@ -164,6 +164,29 @@ public class LinkedLists {
         }
         head = prev;
     }
+    public void deleteNthFromEnd(int n)
+    {
+        //calculate size 
+        int sz = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+        if(n == sz){
+            head = head.next;
+            return;
+        }
+        int i = 1;
+        int iToFind = sz - n;
+        Node prev = head;
+        while(i <iToFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
     public static void main(String[] args) {
         LinkedLists ll = new LinkedLists();
         ll.addFirst(1);
@@ -201,6 +224,10 @@ public class LinkedLists {
         }
         ll.printLL();
         ll.reverse();
+        ll.printLL();
+        ll.reverse();
+        ll.printLL();
+        ll.deleteNthFromEnd(3);
         ll.printLL();
     }
 }
