353485de286d08687e3aef30343fbd6555210ca9
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-08-29 15:37:00 +0530

    Pallindrome a number

---
+++ LeetCode100DayChallagenge/Pallindrome.java
@@ -0,0 +1,28 @@
+
+public class Pallindrome {
+    public static boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int n = x;
+        int revNum = 0;
+        while(n>0)
+        {
+            int d = n%10;
+
+            revNum = revNum * 10 +d;
+
+            n = n/10;
+        }
+        if(revNum == x){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(isPalindrome(121));
+    }
+}
