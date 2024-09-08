c166a4a662d322ba451da29a22dca504dd9f85fc
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-08 23:23:06 +0530

    Linear Search

---
+++ 02-Arrays Programs/PassingArraysAsArg.java
@@ -0,0 +1,17 @@
+public class PassingArraysAsArg {
+    public static void update(int marks[],int nonhangable){
+        nonhangable = 10;
+        for(int i=0; i<marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
+    }
+    public static void main(String[] args) {
+        int marks[] = {97,98,99};
+        int nonhangable = 5;
+        update(marks,nonhangable);
+        System.out.println(nonhangable);
+        for(int i=0; i<marks.length;i++){
+            System.out.println("Print marks ::"+marks[i]+" ");
+        }
+    }
+}
