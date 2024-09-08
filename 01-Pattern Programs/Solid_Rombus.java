47a6f261dd47c961cf1e014c484bf30dcaee8668
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-08 21:16:01 +0530

    Folder Structure updated

---
+++ 01-Pattern Programs/Solid_Rombus.java
@@ -0,0 +1,9 @@
+public class Solid_Rombus {
+    public static void solid_rombus(int n){
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Solid Rhombus");
+        solid_rombus(5);
+    }
+}
