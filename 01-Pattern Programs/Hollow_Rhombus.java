095bf834787048027a58a01ba3f78aca6873ad5a
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-08 21:32:28 +0530

    hollow_rhombus

---
+++ 01-Pattern Programs/Hollow_Rhombus.java
@@ -0,0 +1,24 @@
+public class Hollow_Rhombus {
+    public static void hollow_rhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            // hollow rectangle
+            for (int j = 1; j <= n; j++) {
+                if (j == 1 || j == n ||i==1 || i==n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hollow Rhombus");
+        hollow_rhombus(10);
+    }
+}
