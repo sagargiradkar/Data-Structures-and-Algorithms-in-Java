dd575ada2cb2878e076f6b6dd37c594d47c73afd
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-09 22:24:55 +0530

    creation of 2D array

---
+++ 02-Arrays Programs/2D-Array/Creation_of_2D_Array.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Creation_of_2D_Array{
+    public static void main(String[] args){
+        int matrix[][] = new int[3][3];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size :: ");
+        int n = sc.nextInt();
+        for(int i=0; i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("Enter element at index "+i+j + ":: "); 
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
