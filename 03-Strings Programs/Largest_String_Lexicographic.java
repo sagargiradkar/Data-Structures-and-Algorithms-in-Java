757a29d22fe030406002c7a62b7cbf3f6688b40e
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-10 19:08:57 +0530

    Largest_String_Lexicographic

---
+++ 03-Strings Programs/Largest_String_Lexicographic.java
@@ -0,0 +1,13 @@
+public class Largest_String_Lexicographic {
+    public static void main(String[] args) {
+        String friuts[] = {"apple","mango","banana"};
+
+        String largest = friuts[0];
+        for(int i=1; i<friuts.length; i++){
+            if(largest.compareTo(friuts[i])<0){
+                largest = friuts[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
