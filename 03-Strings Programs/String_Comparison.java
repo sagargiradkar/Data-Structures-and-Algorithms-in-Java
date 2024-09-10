ad77664387caab093f1e0f845c8c22e7a50f0fb0
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-10 18:54:53 +0530

    String_Comparison

---
+++ 03-Strings Programs/String_Comparison.java
@@ -0,0 +1,25 @@
+public class String_Comparison {
+    public static void main(String[] args) {
+        String s1 = "Tony";
+        String s2 = "Tony";
+        String s3 = new String("Tony");
+
+        if(s1==s2){
+            System.out.println("String are equal");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+
+        if(s1 == s3 ){
+            System.out.println("String are equal");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+        if(s1.equals(s3) ){
+            System.out.println("String are equal");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+        
+    }
+}
