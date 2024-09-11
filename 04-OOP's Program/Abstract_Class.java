d44d6dcc0b1845f244b3873821394723ac3db324
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-11 15:45:04 +0530

    Abstract_Class

---
+++ 04-OOP's Program/Abstract_Class.java
@@ -0,0 +1,29 @@
+
+abstract class Animal {
+    public abstract void makeSound(); // Abstract method
+
+    public void eat() {
+        System.out.println("Eating...");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Woof!");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Meow!");
+    }
+}
+public class Abstract_Class {
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        c.makeSound();
+        c.eat();
+    }   
+}
