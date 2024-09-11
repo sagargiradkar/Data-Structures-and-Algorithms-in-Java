58cb0fb0112d4df4d269462a0d59a5a75521275d
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-11 15:39:23 +0530

    Inheriatance

---
+++ 04-OOP's Program/Inheritance.java
@@ -0,0 +1,15 @@
+class Animal{
+    String color;
+    int legs;
+}
+class Dog extends Animal{
+    String name;
+    
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.color = "black";
+        
+    }
+}
