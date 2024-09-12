02fd2f36a2b15ccbc906c46e9586ff674e8745c7
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-12 10:30:45 +0530

    factorial of n

---
+++ 05-Recursion/Factorial_of_N.java
@@ -0,0 +1,17 @@
+public class Factorial_of_N {
+    public static int factorial(int n){
+        
+        if(n == 0){
+            return 1;
+        }
+        int fnm1 = factorial(n-1);
+        int fn = n * factorial(n-1);
+       return fn;
+
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int fact = factorial(n);
+        System.out.println("facctorial of "+n+" is :: "+fact);
+    }
+}
