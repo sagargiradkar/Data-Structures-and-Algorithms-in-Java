f0e43169782ba4e70e4c6e4b70517cd04802527e
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-12 11:40:46 +0530

    public class Last_Occurance

---
+++ 05-Recursion/Last_Occurance.java
@@ -0,0 +1,16 @@
+public class Last_Occurance {
+    public static int lastOccurance(int arr[],int key,int i){
+        if(i == arr.length){
+            return -1;
+        }
+        int isFound = lastOccurance(arr, key, i+1);
+        if(isFound == -1 && arr[i]== key){
+            return i;
+        }
+        return isFound;
+    }
+    public static void main(String[] args) {
+        int arr[] = {8,3,6,9,5,10,2,5,3};
+        System.out.println(lastOccurance(arr, 5, 0));
+    }
+}
