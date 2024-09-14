3b3a7908da8e04d4e5a5302a1f646d627fb84e26
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-14 11:32:16 +0530

    Find_Permulations

--- 09-Backtraking/Find_Permulations.java
+++ 09-Backtraking/Find_Permulations.java
@@ -1,5 +1,22 @@
 public class Find_Permulations {
+    public static void findPermutations(String str,String ans){
+
+        //base case
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        //recursion
+        for(int i=0; i<str.length(); i++){
+            char curr = str.charAt(i);
+            //"abcde" = "ab" + "de" = "abde"
+            String Newstr = str.substring(0,i) + str.substring(i+1);
+            findPermutations(Newstr, ans+curr);
+
+        }
+    }
     public static void main(String[] args) {
-        
+       String str = "abc"; 
+       findPermutations(str, "");
     }
 }
