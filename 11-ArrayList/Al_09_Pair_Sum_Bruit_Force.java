172a8d48c14cc65749d3651ceeb8657f2e4389c9
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-15 17:49:47 +0530

    Al_09_Pair_Sum_Bruit_Force

---
+++ 11-ArrayList/Al_09_Pair_Sum_Bruit_Force.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Al_09_Pair_Sum_Bruit_Force {
+    public static boolean pairSum1(ArrayList<Integer> list, int target){
+        for(int i=0; i<list.size(); i++){
+            for(int j=i+1; j<list.size(); j++){
+                if(list.get(i) + list.get(j) == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 5;
+        System.out.println("Pairs exists :: "+pairSum1(list, target));
+
+    }
+}
