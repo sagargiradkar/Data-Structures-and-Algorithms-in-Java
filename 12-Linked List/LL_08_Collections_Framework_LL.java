23eaed16c07be17f279768cac1c613609fade7d4
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-16 09:27:52 +0530

    LL_08_Collections_Framework_LL

---
+++ 12-Linked List/LL_08_Collections_Framework_LL.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+public class LL_08_Collections_Framework_LL {
+    public static void main(String[] args) {
+        //create - objcts 
+        LinkedList<Integer> ll = new LinkedList<>();
+
+
+        //add
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+
+        //print 
+        System.out.println(ll);
+
+        //remove
+        ll.removeLast();
+        ll.removeFirst();
+
+        System.out.println(ll);
+    }
+}
