13cfcf2eb8c15516020327041a47e3dcb5d59f85
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-16 15:40:26 +0530

    LL_10_Zig_Zag_LL

--- 12-Linked List/LL_10_Zig_Zag_LL.java
+++ 12-Linked List/LL_10_Zig_Zag_LL.java
@@ -1,5 +1,95 @@
 public class LL_10_Zig_Zag_LL {
+
+    // Define the Node class
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // LinkedList class
+    public static class LinkedList {
+        public Node head = null;
+
+        // Method to add elements at the beginning
+        public void addFirst(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+            } else {
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+        // Method to print the linked list
+        public void print() {
+            if (head == null) {
+                System.out.println("List is empty");
+                return;
+            }
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+
+        // Zig-zag method for alternating merging
+        public void zigZag() {
+            // find middle
+            Node slow = head;
+            Node fast = head.next;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            Node mid = slow;
+
+            // reverse 2nd half
+            Node curr = mid.next;
+            mid.next = null;
+            Node prev = null;
+            Node next;
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            // alternate merging -- zig-zag merge
+            Node left = head;
+            Node right = prev;
+            Node nextL, nextR;
+
+            while (left != null && right != null) {
+                nextL = left.next;
+                left.next = right;
+                nextR = right.next;
+                right.next = nextL;
+
+                left = nextL;
+                right = nextR;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+
+        ll.print(); // Print before zig-zag
+        ll.zigZag(); // Perform zig-zag operation
+        ll.print(); // Print after zig-zag
     }
 }
