bc9f48f8e7ce9efabf02249363b00708d8db7199
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-16 08:50:05 +0530

    public class LL_06_Detect_Cycle_Loop {

---
+++ 12-Linked List/LL_06_Detect_Cycle_Loop.java
@@ -0,0 +1,34 @@
+public class LL_06_Detect_Cycle_Loop {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head = null;
+    public static Node tail = null;
+
+    public static boolean isCycle(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast !=null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+        System.out.println(isCycle());
+
+    }
+}
