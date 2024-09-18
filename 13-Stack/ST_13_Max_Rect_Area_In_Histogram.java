e3799144d19d452be469ba6a132bb43c23d691cd
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-18 12:38:36 +0530

    ST_13_Max_Rect_Area_In_Histogram

---
+++ 13-Stack/ST_13_Max_Rect_Area_In_Histogram.java
@@ -0,0 +1,55 @@
+import java.util.Stack;
+
+public class ST_13_Max_Rect_Area_In_Histogram {
+    public static void maxArea(int arr[]) {
+        int maxArea = 0;
+        int nsr[] = new int[arr.length];
+        int nsl[] = new int[arr.length];
+
+        // Next smaller right
+        Stack<Integer> s = new Stack<>();
+
+        // Loop to find the next smaller element to the right
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsr[i] = arr.length;
+            } else {
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // Next smaller left
+        s = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // Calculate the maximum area
+        for (int i = 0; i < arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+
+        System.out.println("Max area in Histogram is :: " + maxArea);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 1, 5, 6, 2, 3};
+        maxArea(arr);
+    }
+}
