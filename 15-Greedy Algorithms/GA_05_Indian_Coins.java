ab30c1d7957769fea628471ddba616b113ca6357
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-25 17:28:15 +0530

    GA_05_Indian_Coins

---
+++ 15-Greedy Algorithms/GA_05_Indian_Coins.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class GA_05_Indian_Coins {
+
+    public static void main(String[] args) {
+        Integer coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 2000};
+
+        Arrays.sort(coins, Comparator.reverseOrder());
+
+        int countOfCoins = 0;
+        int amount = 999;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= amount) {
+                while (coins[i] <= amount) {
+                    countOfCoins++;
+                    ans.add(coins[i]);
+                    amount -= coins[i];
+                }
+            }
+        }
+        System.out.println("Total (min) coins used :: " + countOfCoins);
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
