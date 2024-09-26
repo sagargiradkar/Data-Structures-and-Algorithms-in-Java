661bea4b1dcf82cddb5718e3c713f9899c287376
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-26 15:54:51 +0530

    BT_09_Top_View_Tree

---
+++ 16-BinaryTree/BT_09_Top_View_Tree.java
@@ -0,0 +1,94 @@
+
+import java.util.*;
+
+public class BT_09_Top_View_Tree {
+
+    public static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    static class Info {
+
+        Node node;
+        int hd;
+
+        public Info(BT_09_Top_View_Tree.Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+
+    }
+
+    public static void topView(Node root) {
+        //level order Traversal
+        Queue<Info> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+
+        int min = 0, max = 0;
+        q.add(new Info(root, 0));
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Info curr = q.remove();
+            if (curr == null) {
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if (!map.containsKey(curr.hd)) {
+                    map.put(curr.hd, curr.node);
+                }
+
+                if (curr.node.left != null) {
+                    q.add(new Info(curr.node.left, curr.hd - 1));
+                    min = Math.min(min, curr.hd - 1);
+                }
+
+                if (curr.node.right != null) {
+                    q.add(new Info(curr.node.right, curr.hd + 1));
+                    max = Math.max(max, curr.hd + 1);
+                }
+            }
+        }
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i).data + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println();
+        System.out.print("Tree inorder of the tree: ");
+        inorder(root);
+        System.out.println();
+        topView(root);
+    }
+}
