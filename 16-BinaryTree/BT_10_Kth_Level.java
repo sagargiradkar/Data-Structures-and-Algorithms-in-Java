bca8b986fa1083f89983dec5bc2601314583fd61
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-26 16:02:29 +0530

    BT_10_Kth_Level

---
+++ 16-BinaryTree/BT_10_Kth_Level.java
@@ -0,0 +1,55 @@
+
+public class BT_10_Kth_Level {
+
+    public static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void kLevel(Node root, int level, int k) {
+        if (root == null) {
+            return;
+        }
+        if (level == k) {
+            System.out.print(root.data + " ");
+            return;
+        }
+
+        kLevel(root.left, level + 1, k);
+        kLevel(root.right, level + 1, k);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println();
+        System.out.print("Tree inorder of the tree: ");
+        inorder(root);
+        int k = 2;
+        System.out.println();
+        kLevel(root, 1, k);
+    }
+}
