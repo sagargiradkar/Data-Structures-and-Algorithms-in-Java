19392c485a0bcb4fa0483dd93b52c517090c4fb2
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-09-27 22:43:28 +0530

    HP_02_Insert_Heap

---
+++ 18-Heaps/HP_02_Insert_Heap.java
@@ -0,0 +1,71 @@
+
+import java.util.ArrayList;
+
+public class HP_02_Insert_Heap {
+
+    static class Heap {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        public void add(int data) {
+            //add at last idx
+            arr.add(data);
+
+            int x = arr.size() - 1;//child idx
+            int par = (x - 1) / 2;// par idx
+
+            while (arr.get(x) < arr.get(par)) {
+                int temp = arr.get(x);
+                arr.set(x, arr.get(par));
+                arr.set(par, temp);
+            }
+        }
+
+        public int peek() {
+            return arr.get(0);
+        }
+
+        private void heapify(int idx) {
+            int left = 2 * idx + 1;
+            int right = 2 * idx + 2;
+            int minIdx = idx;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if (minIdx != idx) {
+                int temp = arr.get(idx);
+                arr.set(idx, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+
+            //step1 - swap first & last
+            int temp = arr.get(0);
+            arr.set(0, arr.size() - 1);
+            arr.set(arr.size() - 1, temp);
+
+            //step2 - delete last 
+            arr.remove(arr.size() - 1);
+
+            //step 3 - heapify
+            heapify(0);
+            return data;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
