1b7972c4e992dc89b7467d87596fce1d426dff67
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-10-01 15:43:05 +0530

    Tr_06_Count_Unique_SubString

---
+++ 20-Tries/Tr_06_Count_Unique_SubString.java
@@ -0,0 +1,51 @@
+
+public class Tr_06_Count_Unique_SubString {
+
+    static class Node {
+
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static int countNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int count =0;
+        for(int i=0; i<26; i++){
+            if(root.children[i] != null){
+                count+= countNodes(root.children[i]);
+            }
+        }
+        return count+1;
+    }
+    public static void main(String[] args) {
+        String str = "apple";
+        //SUFFIX 
+        for(int i=0; i<str.length(); i++){
+            String sufix = str.substring(i);
+            insert(sufix);
+        }
+        System.out.println(countNodes(root));
+    }
+}
