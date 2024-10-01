1b52126bb8f11eef50c473af5be63dc2c27af632
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-10-01 13:34:44 +0530

    Tr_01_Creating_Trie

---
+++ 20-Tries/Tr_01_Creating_Trie.java
@@ -0,0 +1,31 @@
+public class Tr_01_Creating_Trie{ 
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+        Node(){
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for(int level = 0; level<word.length(); level++){
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+    public static void main(String[] args) {
+        String[] words={"the","a","there","their","any","thee"};
+        for(int i=0; i<words.length;i++){
+            insert(words[i]);
+        }
+    }
+}
