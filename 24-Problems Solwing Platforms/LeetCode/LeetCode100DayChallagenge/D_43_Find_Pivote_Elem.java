1ad7cde815cc7fe450a62cae587785cd33901540
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-10-14 18:48:16 +0530

    D_43_Find_Pivote_Elem

---
+++ 24-Problems Solwing Platforms/LeetCode/LeetCode100DayChallagenge/D_43_Find_Pivote_Elem.java
@@ -0,0 +1,35 @@
+public class D_43_Find_Pivote_Elem {
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] nums = {1, 7, 3, 6, 5, 6};
+        int pivotIndex = solution.pivotIndex(nums);
+        
+        System.out.println("The pivot index is: " + pivotIndex);
+    }
+}
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int rsum = 0;
+
+        for (int ele : nums) {
+            rsum += ele;
+        }
+
+        int lsum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            rsum -= nums[i];
+
+            if (rsum == lsum) {
+                return i;
+            }
+
+            lsum += nums[i];
+        }
+
+        return -1;
+    }
+}
