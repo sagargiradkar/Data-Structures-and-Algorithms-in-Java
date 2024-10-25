ca422d9bf0be116a5a091e410990a3e5b16dab2d
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-10-25 18:12:25 +0530

    Update 10_Octomber_2024

---
+++ 24-Problems Solwing Platforms/LeetCode/POTDLeetCodeSolution/10_Octomber_2024/25_Remove_Sub_Folder.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class 25_Remove_Sub_Folder {
+    
+    static class Solution {
+        public List<String> removeSubfolders(String[] folder) {
+            Set<String> folderSet = new HashSet<>(Arrays.asList(folder));
+            List<String> result = new ArrayList<>();
+
+            for (String currFolder : folder) {
+                boolean isSubFolder = false;
+                String tempFolder = currFolder;
+
+                while (!currFolder.isEmpty()) {
+                    int position = currFolder.lastIndexOf('/');  
+                    if (position == -1) break;
+
+                    currFolder = currFolder.substring(0, position);
+
+                    if (folderSet.contains(currFolder)) {
+                        isSubFolder = true;
+                        break;
+                    }
+                }
+
+                if (!isSubFolder) {
+                    result.add(tempFolder);
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] folders = {"/a", "/a/b", "/c/d", "/c/d/e", "/c/f"};
+        Solution solution = new Solution();
+        List<String> result = solution.removeSubfolders(folders);
+
+        System.out.println("Folders after removing sub-folders: " + result);
+    }
+}
