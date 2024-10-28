d74954623a3e54ee4ed85bc3eaa818a5d44f59ef
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-10-28 07:03:02 +0530

    removeDuplicateLettersOptimized

--- 24-Problems Solwing Platforms/LeetCode/LeetCode100DayChallagenge/D_46_Remove_Duplicates.java
+++ 24-Problems Solwing Platforms/LeetCode/LeetCode100DayChallagenge/D_46_Remove_Duplicates.java
@@ -13,8 +13,10 @@ public class D_46_Remove_Duplicates {
         
         Solution solution = new Solution();
         String result = solution.removeDuplicateLetters(input);
+        String resultOptimized = solution.removeDuplicateLettersOptimized(input);
         
-        System.out.println("Result after removing duplicates: " + result);
+        System.out.println("Result after removing duplicates (original): " + result);
+        System.out.println("Result after removing duplicates (optimized): " + resultOptimized);
     }
 }
 
@@ -52,5 +54,36 @@ class Solution {
         }
         
         return result.toString();
+    }
+
+    // New optimized method
+    public String removeDuplicateLettersOptimized(String s) {
+        int[] lastIndex = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        boolean[] present = new boolean[26];
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int idx = ch - 'a';
+
+            if (!present[idx]) {
+                while (!st.isEmpty() && st.peek() > ch && lastIndex[st.peek() - 'a'] > i) {
+                    present[st.pop() - 'a'] = false;
+                }
+                st.push(ch);
+                present[idx] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+
+        return sb.reverse().toString();
     }
 }
