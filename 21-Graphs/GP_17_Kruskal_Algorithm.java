54fbd883e6b75de97518c60d86e2ae0a2a975753
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-11-21 18:15:34 +0530

    GP_16_Disjoint_Set_Union

--- 21-Graphs/GP_17_Kruskal_Algorithm.java
+++ 21-Graphs/GP_17_Kruskal_Algorithm.java
@@ -1,8 +1,40 @@
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class GP_17_Kruskal_Algorithm {
+
+        static int n = 7;
+    static int rank[] = new int[n];
+    static int par[] = new int[n];
+
+    public static void init(){
+        for(int i=0; i<n; i++){
+            par[i] = i;
+        }
+    }
+
+    public static int find(int x){
+        if(x ==par[x]){
+            return x;
+        }
+        return par[x] = find(par[x]);
+    }
+
+    public static void union(int a,int b){
+        int parA = find(a);
+        int parB = find(b);
+
+        if(rank[parA] == rank[parB]){
+            par[parB] = parA;
+            rank[parA]++;
+        }else if(rank[parA] < rank[parB]){
+            par[parA] = parB;
+        }else{
+            par[parB] = parA; 
+        }
+    }
 
     static class Edge implements Comparable<Edge> {
 
