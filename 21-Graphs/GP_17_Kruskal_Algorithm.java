a604542fa45174dd36629b0bc1ef38f80f4775d4
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-11-23 10:42:24 +0530

    GP_17_Kruskal_Algorithm

--- 21-Graphs/GP_17_Kruskal_Algorithm.java
+++ 21-Graphs/GP_17_Kruskal_Algorithm.java
@@ -1,38 +1,36 @@
-
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 public class GP_17_Kruskal_Algorithm {
 
-        static int n = 7;
+    static int n = 7;
     static int rank[] = new int[n];
     static int par[] = new int[n];
 
-    public static void init(){
-        for(int i=0; i<n; i++){
+    public static void init() {
+        for (int i = 0; i < n; i++) {
             par[i] = i;
         }
     }
 
-    public static int find(int x){
-        if(x ==par[x]){
+    public static int find(int x) {
+        if (x == par[x]) {
             return x;
         }
         return par[x] = find(par[x]);
     }
 
-    public static void union(int a,int b){
+    public static void union(int a, int b) {
         int parA = find(a);
         int parB = find(b);
 
-        if(rank[parA] == rank[parB]){
+        if (rank[parA] == rank[parB]) {
             par[parB] = parA;
             rank[parA]++;
-        }else if(rank[parA] < rank[parB]){
+        } else if (rank[parA] < rank[parB]) {
             par[parA] = parB;
-        }else{
-            par[parB] = parA; 
+        } else {
+            par[parB] = parA;
         }
     }
 
@@ -54,60 +52,42 @@ public class GP_17_Kruskal_Algorithm {
         }
     }
 
-    // Method to create a graph
-    public static ArrayList<Edge>[] createGraph(int V) {
-        ArrayList<Edge>[] graph = new ArrayList[V];
-
-        for (int i = 0; i < V; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        // Graph edges
-        graph[0].add(new Edge(0, 1, 5));
-
-        graph[1].add(new Edge(1, 0, 5));
-        graph[1].add(new Edge(1, 2, 1));
-        graph[1].add(new Edge(1, 3, 3));
-
-        graph[2].add(new Edge(2, 1, 1));
-        graph[2].add(new Edge(2, 3, 1));
-        graph[2].add(new Edge(2, 4, 4));
-
-        graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 2, 1));
-
-        graph[4].add(new Edge(4, 2, 2));
-
-        return graph;
+    // Method to create a graph by populating the edge list
+    public static void createGraph(ArrayList<Edge> edges) {
+        edges.add(new Edge(0, 1, 5));
+        edges.add(new Edge(1, 2, 1));
+        edges.add(new Edge(1, 3, 3));
+        edges.add(new Edge(2, 3, 1));
+        edges.add(new Edge(2, 4, 4));
+        edges.add(new Edge(4, 2, 2));
     }
 
     public static void kruskalMST(ArrayList<Edge> edges, int V) {
         init();
-        Collections.sort(edges);
+        Collections.sort(edges); // Sort edges by weight
         int mstCost = 0;
         int count = 0;
 
-        for(int i=0; count <V-1; i++){
-          Edge e = edges.get(i);
+        for (int i = 0; count < V - 1; i++) {
+            Edge e = edges.get(i);
 
-          int parA = find(e.src);
-          int parB = find(e.dest);
+            int parA = find(e.src);
+            int parB = find(e.dest);
 
-          if(parA != parB){
-            union(e.src,e.dest);
-            mstCost += e.wt;
-            count++;
-          }
+            if (parA != parB) {
+                union(e.src, e.dest);
+                mstCost += e.wt;
+                count++;
+            }
         }
 
-        System.out.println(mstCost);
-
+        System.out.println("Minimum Cost of the MST: " + mstCost);
     }
 
     public static void main(String[] args) {
-        int V = 4;
+        int V = 5; // Number of vertices in the graph
         ArrayList<Edge> edges = new ArrayList<>();
-        createGraph(edges);
+        createGraph(edges); // Populate the edge list
         kruskalMST(edges, V);
     }
 }
