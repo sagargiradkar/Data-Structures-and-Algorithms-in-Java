f81e9443427defc0be9477b45d689c6c56aa19b6
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-11-25 08:33:03 +0530

    DP_01_Fibbonaci_Series

--- 22-Dynamic Programming/DP_01_Fibbonaci_Series.java
+++ 22-Dynamic Programming/DP_01_Fibbonaci_Series.java
@@ -1,15 +1,21 @@
 
 public class DP_01_Fibbonaci_Series {
 
-    public static int fib(int n) {
+    public static int fib(int n, int f[]) {
         if (n == 0 || n == 1) {
             return n;
         }
-        return fib(n - 1) + fib(n - 2);
+        if (f[n] != 0) {
+            return f[n];
+        }
+        f[n] = fib(n - 1, f) + fib(n - 2, f);
+        return f[n];
     }
 
     public static void main(String[] args) {
-        int n = 5;
-        System.out.println(fib(n));
+        int n = 10;
+        int f[] = new int[n + 1];
+
+        System.out.println(fib(n, f));
     }
 }
