6a7e82d8844bad761663baf57a1a03f0079c1526
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-11-27 19:48:37 +0530

    DP_03_Climbing_Stairs_Memoization

--- 22-Dynamic Programming/DP_03_Climbing_Stairs_Memoization.java
+++ 22-Dynamic Programming/DP_03_Climbing_Stairs_Memoization.java
@@ -1,7 +1,10 @@
 
+import java.util.Arrays;
+
+
 public class DP_03_Climbing_Stairs_Memoization {
 
-    public static int countWays(int n) {
+    public static int countWays(int n,int ways[]) {
 
         if (n == 0) {
             return 1;
@@ -9,11 +12,17 @@ public class DP_03_Climbing_Stairs_Memoization {
         if (n < 0) {
             return 0;
         }
-        return countWays(n - 1) + countWays(n - 2);
+        if(ways[n] != -1){
+            return ways[n];
+        }
+        ways[n] = countWays(n - 1,ways) + countWays(n - 2,ways);
+        return ways[n];
     }
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(countWays(n));
+        int ways[] = new int[n+1];
+        Arrays.fill(ways, -1);
+        System.out.println(countWays(n,ways));
     }
 }
