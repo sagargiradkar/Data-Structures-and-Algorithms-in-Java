6f6c0bdbdc2381cced24f280e877cd3967bb9307
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2024-12-10 16:27:55 +0530

    D_10_Maximum_Length

---
+++ 24-Problems Solwing Platforms/LeetCode/POTDLeetCodeSolution/12_December_2024/D_10_Maximum_Length.java
@@ -0,0 +1,46 @@
+public class D_10_Maximum_Length {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "aaabbbaaa";
+        System.out.println("Maximum Length: " + solution.maximumLength(s));
+    }
+}
+
+class Solution {
+    public int maximumLength(String s) {
+        int n = s.length();
+        int[][] matrix = new int[26][n + 1]; // Matrix to store counts for each character and length
+        char prevChar = s.charAt(0);
+        int length = 0;
+
+        // Populate the matrix
+        for (int i = 0; i < n; i++) {
+            char currChar = s.charAt(i);
+
+            if (currChar == prevChar) {
+                length += 1;
+                matrix[currChar - 'a'][length] += 1;
+            } else {
+                length = 1;
+                matrix[currChar - 'a'][length] += 1;
+                prevChar = currChar;
+            }
+        }
+
+        int result = 0;
+
+        // Calculate the result by checking cumulative sums in reverse order
+        for (int i = 0; i < 26; i++) {
+            int cumSum = 0;
+            for (int j = n; j >= 1; j--) {
+                cumSum += matrix[i][j];
+                if (cumSum >= 3) {
+                    result = Math.max(result, j);
+                    break;
+                }
+            }
+        }
+
+        return result == 0 ? -1 : result;
+    }
+}
