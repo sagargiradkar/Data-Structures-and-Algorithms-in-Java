dee313b63b48b26687c4553b91d43fda27427d47
Sagar Giradkar <spgiradkar2002@gmail.com>
2025-03-05 18:44:26 +0530

    sum of first N fibonacci Elements

---
+++ 25-Companies Specific Questions/06-TCS/D_01_Find_Sum.java
@@ -0,0 +1,32 @@
+
+//find the sum of first N fibonacci Elements
+import java.util.Scanner;
+
+public class D_01_Find_Sum {
+
+    public static int fibonacciSum(int n) {
+
+        int a = 0;
+        int b = 1;
+        int sum = a + b;
+        int temp;
+        for (int i = 2; i < n; i++) {
+            temp = a + b;
+            sum = sum + temp;
+            a = b;
+            b = temp;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("Enter N :: ");
+        int n = obj.nextInt();
+
+        System.out.println("Sum of first fibonacci :: " + fibonacciSum(n));
+
+    }
+}
