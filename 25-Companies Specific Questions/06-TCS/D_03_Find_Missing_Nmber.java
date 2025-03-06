e5893b1dcbfab5586408689296de272b7d1e5a3a
Sagar Giradkar <spgiradkar2002@gmail.com>
2025-03-06 18:33:03 +0530

    D_03_Find_Missing_Nmber

--- 25-Companies Specific Questions/06-TCS/D_03_Find_Missing_Nmber.java
+++ 25-Companies Specific Questions/06-TCS/D_03_Find_Missing_Nmber.java
@@ -9,11 +9,27 @@ public class D_03_Find_Missing_Nmber {
         for (int x : arr) {
             result = result + x;
         }
-        return s-result;
+        return s - result;
+    }
+
+    public static int missingXOR(int[] arr) {
+        int n = arr.length;
+        int totalXORSum = 0;
+        int totalXORArraySum = 0;
+
+        for (int i = 0; i <= n; i++) {
+            totalXORSum = totalXORSum ^ i;
+        }
+
+        for (int x : arr) {
+            totalXORArraySum = totalXORArraySum ^ x;
+        }
+        return totalXORSum ^ totalXORArraySum;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 3, 0, 1 };
-        System.out.println(missingNum(arr));
+        int[] arr = { 3, 0, 1, 2, 5 };
+        System.out.println("Using Iterations :: " + missingNum(arr));
+        System.out.println("Using XOR :: " + missingXOR(arr));
     }
 }
